96ff7eeb1451be67360541490de0798c1387d845
Oleg Angulyarov <4nglrv@gmail.com>
2023-02-18 19:30:22 +0300

    Added throttle.ts

---
+++ src/utils/throttle.ts
@@ -0,0 +1,9 @@
+export default function throttle(func: (...args: any) => void, ms: number) {
+  let timeout = false;
+  return (...args: any) => {
+    if (timeout) return
+    func(...args)
+    timeout = true
+    setTimeout(() => timeout = false, ms)
+  }
+}
